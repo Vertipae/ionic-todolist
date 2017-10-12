cd759098e7cbcb0543a82585d36fec793b42ae4f
futstone <hairetdin.nico@gmail.com>
2017-10-12 16:52:58 +0300

    added loginPage and registerPage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
+import { LoginPage } from '../login/login';
+
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('username') username;
+  @ViewChild('password') password;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+  }
 
+  login() {
+    this.navCtrl.push('LoginPage');
+  }
+
+  register() {
+    this.navCtrl.push('RegisterPage');
   }
 
 }
---
+++ src/pages/login/login.ts
@@ -0,0 +1,35 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  @ViewChild('username') username;
+  @ViewChild('password') password;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  signIn() {
+    if (this.username.value === "admin" && this.password.value === "admin") {
+      this.showAlert();
+    }
+  }
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'New Friend!',
+      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+}
